3d4b9ba81582d10114942b1af559ed99cc5cd12b
bara8383 <130632770+bara8383@users.noreply.github.com>
2026-06-15 02:29:07 +0900

    make-backup

--- packages/app/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { createApp } from '@backstage/frontend-defaults';
-import catalogPlugin from '@backstage/plugin-catalog/alpha';
-import { appearanceModule } from './modules/appearance';
-import { navModule } from './modules/nav';
-
-export default createApp({
-  features: [catalogPlugin, appearanceModule, navModule],
-});
